4db205ab7b3fdc747ee4d3adc8fb07bd3eb67785
Ji Insung <49591292+JIINSUNG@users.noreply.github.com>
2025-01-20 16:29:25 +0900

    [Bronze III] Title: 약수 구하기, Time: 96 ms, Memory: 109544 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.py
@@ -0,0 +1,15 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+factors = []
+n, k = map(int, input().split())
+
+
+for i in range(1, n+1):
+    if n % i == 0:
+        factors.append(i)
+
+if len(factors) < k:
+    print(0)
+else:
+    print(factors[k-1])
