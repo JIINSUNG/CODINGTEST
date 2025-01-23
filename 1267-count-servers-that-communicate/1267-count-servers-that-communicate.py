46f3ea2153eadf0b0b49ad47dc38653bae1bfbb0
Ji Insung <49591292+JIINSUNG@users.noreply.github.com>
2025-01-23 23:52:39 +0900

    Time: 740 ms (5.24%), Space: 19.7 MB (22.47%) - LeetHub

--- 1267-count-servers-that-communicate/1267-count-servers-that-communicate.py
+++ 1267-count-servers-that-communicate/1267-count-servers-that-communicate.py
@@ -9,21 +9,18 @@ class Solution:
                     col.append(j)
 
         connects = [False] * len(row)
-        row.sort()
-        col.sort()
-        previous = row[0]
-        for i in range(1, len(row)):
-            if row[i] == previous:
-                connects[i-1] = True
-                connects[i] = True
-            previous = row[i]
+
+        for i in range(len(row)):
+            for j in range(i+1, len(row)):
+                if row[i] == row[j]:
+                    connects[i] = True
+                    connects[j] = True
 
         previous = col[0]
-        for i in range(1, len(col)):
-            if col[i] == previous:
-                connects[i-1] = True
-                connects[i] = True
-            previous = col[i]
-        print(connects)
+        for i in range(len(col)):
+            for j in range(i+1, len(col)):
+                if col[i] == col[j]:
+                    connects[i] = True
+                    connects[j] = True
         return connects.count(True)
 
