6237fce93b084c7d9e9ef1e0f10dfe2ca2a5072b
Ji Insung <49591292+JIINSUNG@users.noreply.github.com>
2025-02-09 20:50:03 +0900

    Time: 178 ms (50.72%), Space: 95.8 MB (25.84%) - LeetHub

--- 2349-design-a-number-container-system/2349-design-a-number-container-system.py
+++ 2349-design-a-number-container-system/2349-design-a-number-container-system.py
@@ -1,30 +1,41 @@
 from collections import defaultdict
-import heapq
 
 class NumberContainers:
 
     def __init__(self):
-        self.idx_value = {}  
-        self.value_idx = defaultdict(list) 
-        self.check_avail = defaultdict(lambda: defaultdict(bool)) 
+        self.idx_value = { 1 : 10} # { idx : value }
+        
+        self.value_idx = defaultdict(list) # {10 : heap : [idx1, idx2, ...]}
+
+        self.check_avail = defaultdict(lambda: defaultdict(bool)) # { 10 : {1 : False, 2 : True}}
 
     def change(self, index: int, number: int) -> None:
         if index in self.idx_value:
-            old_number = self.idx_value[index]
-            self.check_avail[old_number][index] = False
+            self.check_avail[self.idx_value[index]][index] = False # 이전 인덱스 유효성을 False로 변경
         
         self.idx_value[index] = number
         self.check_avail[number][index] = True
         heapq.heappush(self.value_idx[number], index)
 
+
+
     def find(self, number: int) -> int:
         if not self.value_idx[number]:
             return -1
-        
+            
         while self.value_idx[number]:
             idx = heapq.heappop(self.value_idx[number])
-            if self.check_avail[number][idx]:  
-                heapq.heappush(self.value_idx[number], idx)  
-                return idx
-        
+            if self.check_avail[number][idx]:
+                heapq.heappush(self.value_idx[number], idx)
+                return idx 
+
         return -1
+
+        
+# ["NumberContainers", "find", "change", "change", "change", "change", "find", "change", "find"]
+# [[], [10], [2, 10], [1, 10], [3, 10], [5, 10], [10], [1, 20], [10]]
+
+# Your NumberContainers object will be instantiated and called as such:
+# obj = NumberContainers()
+# obj.change(index,number)
+# param_2 = obj.find(number)
