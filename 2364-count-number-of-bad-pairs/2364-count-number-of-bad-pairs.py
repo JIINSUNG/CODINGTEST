8fab63f3c894e90a1d0c4fea6a11943823928b1b
Ji Insung <49591292+JIINSUNG@users.noreply.github.com>
2025-02-09 16:41:56 +0900

    Time: 151 ms (5.47%), Space: 35.6 MB (99.29%) - LeetHub

--- 2364-count-number-of-bad-pairs/2364-count-number-of-bad-pairs.py
+++ 2364-count-number-of-bad-pairs/2364-count-number-of-bad-pairs.py
@@ -1,13 +1,28 @@
 class Solution:
     def countBadPairs(self, nums: List[int]) -> int:
-        # j가 i보다 크고 
-        # j - i != nums[j] - nums[i]이면 bad pair 이다
-        # bad pair의 총 갯수를 출력하라
-        cnt = 0 
+
         for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if j-i != nums[j] - nums[i]:
-                    cnt += 1
+            nums[i] = i - nums[i]
+        dictionary = defaultdict(int)
+
+        for num in nums:
+            dictionary[num] += 1
+        total = len(nums)
+        cnt = 0
+        for i in range(len(nums)):
+            dictionary[nums[i]]-= 1
+            total -= 1
+            cnt += total - dictionary[nums[i]]
         return cnt
+
+    
+
+
+        
+
+        
+
+        
+
 
 
