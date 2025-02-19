90b2f81ac3e3becdaec68b339944bcd97a5d85cb
Ji Insung <49591292+JIINSUNG@users.noreply.github.com>
2025-02-20 00:25:46 +0900

    Time: 612 ms (11.6%), Space: 17.8 MB (92.27%) - LeetHub

--- 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.py
+++ 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.py
@@ -1,8 +1,7 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         for i in range(len(s) // 3):
-            s = s.split('abc')
-            s = ''.join(s)
+            s = s.replace('abc', '')
         
         if s:
             return False
