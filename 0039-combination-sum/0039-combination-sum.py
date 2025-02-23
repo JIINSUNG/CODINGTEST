f432e00961a2a44a7ab86340e29447d97ce64119
Ji Insung <49591292+JIINSUNG@users.noreply.github.com>
2025-02-23 22:46:23 +0900

    Time: 32 ms (10.66%), Space: 18.1 MB (19.83%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -4,26 +4,26 @@ class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         dictionary = defaultdict(list)
         candidates.sort()
-
-        def dfs(index, current_sum, path):
-            if current_sum == target:
-                dictionary[target].append(path[:])
-                return
-            
-            if current_sum > target:
-                return
-            
-            for i in range(index, len(candidates)):
-                dfs(i, current_sum + candidates[i], path + [candidates[i]])
-
         for candi in candidates:
             value = candi
             while value <= target:
-                dictionary[value].append([candi] * (value//candi))
+                dictionary[value].append([candi] * (value//candi)) 
                 value += candi
+        
+        for i in range(1, target+1):
+            # i를 만든다고 생각하자
+            for candi in candidates:
+                if candi >= i or not candi:
+                    break
 
-        dfs(0, 0, [])
+                value = candi
+                while value <= i:
+                    if i - value < 0:
+                        break
 
-        dictionary[target] = [list(t) for t in set(tuple(sorted(x)) for x in dictionary[target])]
-
+                    for lists in dictionary[i-value]:
+                        temp = sorted(lists + [candi] * (value//candi))
+                        if temp not in dictionary[i]:
+                            dictionary[i].append(temp) 
+                    value += candi
         return dictionary[target]
