92e0882a86bdcd1dfbb14a2e4dd7d0e3265d428a
Ji Insung <49591292+JIINSUNG@users.noreply.github.com>
2025-02-24 22:51:45 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0592-fraction-addition-and-subtraction/0592-fraction-addition-and-subtraction.py
@@ -0,0 +1,50 @@
+import math
+class Solution:
+    def fractionAddition(self, expression: str) -> str:
+        # 분수 덧셈과 뺄셈의 표현을 나타내는 문자열 표현식이 주어졌을 때, 계산 결과를 문자열 형식으로 반환합니다.
+        # 최종 결과는 기약 분수여야 합니다. 최종 결과가 정수인 경우 분모가 1인 분수 형식으로 변경합니다. 따라서 이 경우 2는 2/1로 변환해야 합니다.
+        분자 = []
+        분모 = []
+        
+        "-1/2+1/2"
+        isMinus = False
+        분자차례 = True
+        for i in range(len(expression)):
+
+            if expression[i] == '-':
+                isMinus = True
+            # 숫자야?
+            elif expression[i].isdigit():
+                # 분자 차례인지 체크, 마이너스 기호 있는지 체크
+                if 분자차례:
+                    if isMinus:
+                        분자.append(-int(expression[i]))
+                        isMinus = False
+                    else:
+                        분자.append(int(expression[i]))
+                else:
+                    분모.append(int(expression[i]))
+                    분자차례 = True
+                
+            
+            elif expression[i] == '/':
+                분자차례 = False
+                
+        분모copy = set(분모)
+        lcm = 1
+        for 인자 in 분모copy:
+            lcm *= 인자         
+        
+        for i in range(len(분자)):
+            분자[i] *= lcm // 분모[i]
+        
+        result = sum(분자)
+        if result == 0:
+            return(f"{result}/1")
+        else:
+            gcd = math.gcd(result, lcm)
+            return(f"{result//gcd}/{lcm//gcd}")
+
+        # 분자, 분모를 통일 시키기
+
+
