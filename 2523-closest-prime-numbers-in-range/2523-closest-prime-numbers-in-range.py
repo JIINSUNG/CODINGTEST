0e1aac86c51f2b78705ec14d86911778686cc1b0
Ji Insung <49591292+JIINSUNG@users.noreply.github.com>
2025-03-08 00:50:26 +0900

    Time: 1566 ms (47.19%), Space: 35.2 MB (18.13%) - LeetHub

---
+++ 2523-closest-prime-numbers-in-range/2523-closest-prime-numbers-in-range.py
@@ -0,0 +1,34 @@
+class Solution:
+    def closestPrimes(self, left: int, right: int) -> List[int]:
+        # left <= num1 < num2 <= right 을 만족하는 
+        # 소수 num1, num2를 찾돼, 그 갭이 최소가 되는 num1, num2 구하기
+        # 만약 없다면 [-1, -1] 리턴
+
+
+        answer = [-1, -1]
+        primes = [True] * (right+1)
+        primes[0] = False
+        primes[1] = False
+
+        candidate = []
+        dist = float('inf')
+
+
+        for i in range(2, int(right**0.5)+1):
+            if primes[i]:
+                for j in range(i*2, right+1, i):
+                    primes[j] = False
+        for i in range(left, right+1):
+            if primes[i]:
+                candidate.append(i)
+
+
+        for i in range(0, len(candidate)-1):
+            if candidate[i+1] - candidate[i] < dist:
+                dist = candidate[i+1] - candidate[i]
+                answer[0] = candidate[i]
+                answer[1] = candidate[i+1]
+        return answer
+
+
+            
