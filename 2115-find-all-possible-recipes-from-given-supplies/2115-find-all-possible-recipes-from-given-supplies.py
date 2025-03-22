bf275b8d941bcc9202208b2762cf6af3b703b9ec
Ji Insung <49591292+JIINSUNG@users.noreply.github.com>
2025-03-22 23:02:32 +0900

    Time: 117 ms (29.42%), Space: 20.5 MB (21.56%) - LeetHub

--- 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.py
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.py
@@ -1,27 +1,29 @@
 class Solution:
     def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
         
-        graph = {}
+        graph = defaultdict(set)
         indegree = {}
         answer = []
         queue = deque()
-        
-        for i in range(len(recipes)):
-            graph[recipes[i]] = ingredients[i]
-            indegree[recipes[i]] = len(ingredients[i])
 
+        for i in range(len(recipes)):
+            for ingredient in ingredients[i]:
+                graph[ingredient].add(recipes[i])
+            indegree[recipes[i]] = len(ingredients[i])
+        
+        print(graph)
         for supply in supplies:
             queue.append(supply)
 
         while queue:
             ingredient = queue.popleft()
 
-            for key,value in graph.items():
-                if ingredient in value:
-                    indegree[key]-= 1
-                    if indegree[key] == 0:
-                        answer.append(key)
-                        queue.append(key)
+            for recipe in graph[ingredient]:
+                indegree[recipe] -= 1
+
+                if indegree[recipe] == 0:
+                    answer.append(recipe)
+                    queue.append(recipe)
         return answer 
                 
 
