98dc15e941fbb278e33a9f7b95d9c7acdca6e1b7
Ji Insung <49591292+JIINSUNG@users.noreply.github.com>
2025-03-22 22:34:27 +0900

    Time: 344 ms (14.18%), Space: 19.2 MB (99.92%) - LeetHub

---
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.py
@@ -0,0 +1,30 @@
+class Solution:
+    def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
+        
+        graph = {}
+        indegree = {}
+        answer = []
+        queue = deque()
+        
+        for i in range(len(recipes)):
+            graph[recipes[i]] = ingredients[i]
+            indegree[recipes[i]] = len(ingredients[i])
+
+        for supply in supplies:
+            queue.append(supply)
+
+        while queue:
+            ingredient = queue.popleft()
+
+            for key in graph.keys():
+                if ingredient in graph[key]:
+                    graph[key].remove(ingredient)
+                    indegree[key]-= 1
+                    if indegree[key] == 0:
+                        answer.append(key)
+                        queue.append(key)
+        return answer 
+                
+
+
+        # print(graph)
