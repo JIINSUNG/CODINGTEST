019e906fa8598c26cdcbbe0264c1b0193a9da3dc
Ji Insung <49591292+JIINSUNG@users.noreply.github.com>
2025-03-26 21:59:24 +0900

    Time: 192 ms (26.67%), Space: 81.3 MB (46.67%) - LeetHub

---
+++ 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[][]} grid
+ * @param {number} x
+ * @return {number}
+ */
+var minOperations = function(grid, x) {
+        grid = grid.flat().sort((a, b) => a-b)
+        let target = grid[Math.floor(grid.length / 2)]
+        console.log(target)
+        let answer = 0
+        for (let g of grid){
+            if((target-g) % x == 0){
+                answer += Math.abs(target - g) / x
+            }
+            else
+                return -1
+        }
+
+        return answer
+};
