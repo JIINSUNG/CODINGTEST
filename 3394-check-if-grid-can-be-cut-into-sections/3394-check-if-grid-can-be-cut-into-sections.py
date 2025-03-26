0b01a9ce5643ed4c5fa4c90970a5ba73a8a519f7
Ji Insung <49591292+JIINSUNG@users.noreply.github.com>
2025-03-26 16:53:27 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3394-check-if-grid-can-be-cut-into-sections/3394-check-if-grid-can-be-cut-into-sections.py
@@ -0,0 +1,25 @@
+class Solution:
+    def checkValidCuts(self, n: int, rectangles: List[List[int]]) -> bool:
+        n = len(rectangles)
+        
+        def is_can(direction):
+            rectangles.sort(key = lambda x : x[direction])
+
+            last_end = rectangles[0][direction+2]
+            cnt = 0
+
+            for rectangle in rectangles[1:len(rectangles)]:
+                # 시작점을 가져온다
+                if rectangle[direction] >= last_end:
+                    cnt += 1
+
+                last_end = max(last_end, rectangle[direction+2])
+            
+            if cnt >= 2:
+                return True
+
+            return False
+        
+        return is_can(0) or is_can/(1) 
+            
+
