cf05cc1a41342095f11b100f1e38123099f33b7c
Ji Insung <49591292+JIINSUNG@users.noreply.github.com>
2025-10-05 00:43:03 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.js
@@ -0,0 +1,17 @@
+function solution(num_list) {
+    var answer = 0;
+    
+    let odd = ''
+    let even = ''
+    
+    for(const num of num_list){
+        if (num % 2 === 0){
+            even += num
+        }else{
+            odd += num
+        }
+    }
+    
+    
+    return +odd + +even
+}
