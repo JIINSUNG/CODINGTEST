db68d67a1e87a7d7b05bad1125d71bb2ffd6730a
Ji Insung <49591292+JIINSUNG@users.noreply.github.com>
2025-10-06 12:35:53 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
@@ -2,5 +2,5 @@ function solution(my_string) {
     var answer = [];
     
     
-    return my_string.match(/\d/g).map(Number).sort((a,b) => a-b)
+    return my_string.match(/[0-9]/g).map(Number).sort((a,b) => a-b)
 }
