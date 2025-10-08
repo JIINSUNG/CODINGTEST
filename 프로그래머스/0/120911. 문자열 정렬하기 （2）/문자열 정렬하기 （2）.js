ce54259f2f96a6e205d1ee89dea11336b269f028
Ji Insung <49591292+JIINSUNG@users.noreply.github.com>
2025-10-08 13:40:46 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.js
@@ -0,0 +1,7 @@
+function solution(my_string) {
+    var answer = '';
+    my_string = my_string.toLowerCase()
+    my_string = my_string.split('')
+    my_string.sort()
+    return my_string.join('');
+}
