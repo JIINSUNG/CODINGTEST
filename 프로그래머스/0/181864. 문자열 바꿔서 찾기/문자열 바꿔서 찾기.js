c08f5b47ec0f1fcfbceb90928c2e0940eec9d0a6
Ji Insung <49591292+JIINSUNG@users.noreply.github.com>
2025-10-10 23:00:02 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.js
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.js
@@ -2,6 +2,6 @@ function solution(myString, pat) {
     var answer = 0;
     
     myString = myString.replaceAll('A', 'C').replaceAll('B','A').replaceAll('C','B')
-    return  myString.indexOf(pat) !== -1 ? 1 : 0
+    return  myString.includes(pat) ? 1 : 0
     
 }
